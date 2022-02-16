91c5bebeb0b47efe9359bf56c6b59be1beeeaae8
Balint Biro <biro.balint.szie@gmail.com>
2022-02-16 13:12:14 +0100

    MySQL server authentications

---
+++ MySQL_authentications.bash
@@ -0,0 +1,6 @@
+#Ensembl
+mysql -h ensembldb.ensembl.org -u anonymous -P 3306
+mysql -h ensembldb.ensembl.org -u anonymous -P 5306
+
+#UCSC
+mysql -u genome -h genome-euro-mysql.soe.ucsc.edu -A -P 3306
